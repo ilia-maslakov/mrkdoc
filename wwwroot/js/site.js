dcd4cc86ad62e192ff6edbd7e1e9854a1ba32263
Ilia Maslakov <il.smind@gmail.com>
2020-09-13 23:48:38 +0300

    clean up

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -27,23 +27,6 @@
         }
     });
 
-    $("#view-rendered").ready(function() {
-        var el = $("#view-rendered");
-        var text = el[0].dataset.searchtext;
-        if (text != '') {
-            var scrollTop = $('#view-rendered').scrollTop();
-            var element = $("#view-rendered :contains('" + text + "')").eq(0);
-            var position = element.position();
-            var offset = scrollTop + position.top;
-
-            element.css('border-left-width', '6px');
-            element.css('border-left-style', 'solid');
-            element.css('border-left-color', 'coral');
-
-            $(window).scrollTop(offset);
-        }
-    });
-
     function insertAtCaret(element, text) {
         console.log("insertAtCaret");
         if (document.selection) {  
